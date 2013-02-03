529b26671eb547f07652dcf42f45fafdfaa8191b
George Tang <georgetang.ca@gmail.com>
2013-02-02 21:49:45 -0500

    created

---
+++ classes/init.php
@@ -0,0 +1,18 @@
+<?php 
+	
+$simple_links_classes = array( 
+                'simple-links-post-type-tax',
+                'simple-links.class' 
+                    );
+
+foreach( $simple_links_classes as $class ){
+	require( $class . '.php' );
+	
+}
+
+$simple_links_func = new simple_links();
+
+if( is_admin() ){
+	require( 'simple-links.admin.class.php' );
+	$simple_links_admin_func = new simple_links_admin();
+}
---
+++ simple-links.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Plugin Name: Pillar Page  Links
+Plugin URI: 
+Description: 
+Version: 1.0
+Author: George Tang
+Author URI: 
+*/
+define( 'SIMPLE_LINKS_IMG_DIR', plugin_dir_url(__FILE__). 'img/' );
+define( 'SIMPLE_LINKS_JS_DIR',  plugin_dir_url(__FILE__). 'js/' );
+define( 'SIMPLE_LINKS_JS_PATH',  plugin_dir_path(__FILE__). 'js/' );
+define( 'SIMPLE_LINKS_CSS_DIR', plugin_dir_url(__FILE__). 'css/' );
+define( 'SIMPLE_LINKS_SHORTCODE_DIR', plugin_dir_path(__FILE__). 'shortcode/' );
+
+require( 'classes/init.php' );
+
+
+
